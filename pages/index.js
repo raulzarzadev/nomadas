6e14a1420809f4e5383afe8b8830cb0dc850d0f6
Raul Zarza <raulzarza.dev@gmail.com>
2020-12-13 22:06:03 -0700

    Ovlide quitar las boxes en el index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import BlackSection from "../src/components/BlackSeccion/BlackSection";
 import MyLayout from "../src/components/layouts/MyLoayout";
+import MyLink from "../src/components/MyLink";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -11,14 +12,8 @@ export default function Home() {
         <title>nomadas creando el camino</title>
       </Head>
       <h1>Nomadasgroup</h1>
-      <h2>Nomadasgroup</h2>
-      <BlackSection />
-      <h1></h1>
-      <BlackSection />
-      <BlackSection />
-      <BlackSection />
-      <BlackSection />
-      <BlackSection />
+      <h2>Pronto estará aquí</h2>
+      <MyLink title="avances acá" href="/visual-guide" />
     </>
   );
 }
